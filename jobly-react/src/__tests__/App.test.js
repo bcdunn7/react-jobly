037682af39a70821a1331f6e1d94481ca76f806e
Bryce <bryce.cc.dunn@gmail.com>
2021-10-25 21:33:47 -0500

    cleanup

--- jobly-react/src/__tests__/App.test.js
+++ jobly-react/src/__tests__/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import { MemoryRouter } from 'react-router';
-import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
     test('it renders without crashing', () => {
